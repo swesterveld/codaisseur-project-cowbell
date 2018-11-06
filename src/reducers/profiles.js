3475afdf6e1d26f80df18559a7db50570116d8c1
Jiri Swen <jiri.ei@hotmail.com>
2018-11-06 15:09:27 +0100

    clean slate

--- src/reducers/profiles.js
+++ src/reducers/profiles.js
@@ -1,12 +1,11 @@
 import dummyInitialState from './dummyData';
-import {setCandidates} from '../actions/candidates'
 
 export default (state = dummyInitialState, action = {}) => {
   switch (action.type) {
     case 'ADD_LIKE':
       const newState = {...state};
       newState[action.payload.fromId].addLike(action.payload.toId);
-      return state;
+      return newState;
     case 'ADD_DISLIKE':
       return state;
     default:
