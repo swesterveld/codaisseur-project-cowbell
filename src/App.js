6a1800336c479a83a89515ed19095e1636be433a
Jiri Swen <jiri.ei@hotmail.com>
2018-11-06 15:13:10 +0100

    adding some actions and a reducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Route} from 'react-router-dom';
-import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
         <Route exact path="/"><p>test</p></Route>
       </div>
     );
