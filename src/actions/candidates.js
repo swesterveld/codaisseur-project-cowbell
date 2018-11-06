1cb6b55bbc9a93e6066a6f53c83b3bd54a6abb6b
Sil Westerveld <git@silwesterveld.com>
2018-11-06 14:04:19 +0100

    Modify action to look for candidates based on profile (instead of id)

--- src/actions/candidates.js
+++ src/actions/candidates.js
@@ -1,11 +1,11 @@
-export const GET_CANDIDATES_FOR_ID = 'GET_CANDIDATES_FOR_ID'
+export const GET_CANDIDATES_FOR_PROFILE = 'GET_CANDIDATES_FOR_PROFILE'
 
-// Action will pass an id to get candidates for
-export function getCandidatesForId(id) {
+// Action will pass a profile to get candidates for
+export function getCandidatesForProfile(profile) {
   return {
-    type: GET_CANDIDATES_FOR_ID
+    type: GET_CANDIDATES_FOR_PROFILE
     payload: {
-      id
+      profile
     }
   }
 }
