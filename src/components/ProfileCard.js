7894a0bc8042c75f062aa7fd131a095c53345529
Sil Westerveld <git@silwesterveld.com>
2018-11-07 15:13:57 +0100

    (WIP) Initial version of ProfileCard, with implemented action to toggle type of active user

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -2,22 +2,65 @@ import React, { Component } from 'react';
 
 class ProfileCard extends Component {
 
-  renderProfile() {
-    if (!this.props.profile) {
-      return <p>No profiles available.</p>
-    }
-    else {
-      return (<div>
-        <p>Name: {this.props.profile.name}</p>
-        <p>expand using object.entries-loop or sth</p>
-      </div>)
-    }
+  renderProfilePicture() {
+    return (
+      <div>
+        <img src={this.props.profile.pictureURL} alt={this.props.profile.name}/>
+        <p>{this.props.profile.name}, {this.props.profile.age}</p>
+        <p>{this.props.profile.location}</p>
+      </div>
+    )
+  }
+
+  renderContent() {
+    return (
+      <div>
+        {this.renderDescription()}
+        {this.renderMediaURLs()}
+      </div>
+    )
+  }
+
+  renderMediaURLs() {
+    return (
+      <div>
+        <h2>Media</h2>
+        <ul>
+          {this.props.profile.recordingURLs.map((item,index) => {
+            return <li><a href={item}>link {index+1}</a></li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+
+  renderDescription() {
+    return (
+      <div>
+        <h2>Description</h2>
+        <p>
+          <span>{this.props.profile.genres}</span>
+          <span>{this.props.profile.roles}</span>
+        </p>
+        <p>{this.props.profile.description}</p>
+      </div>
+    )
   }
 
   render() { 
-    return ( <div>
-      {this.renderProfile()}
-    </div> );
+    if (!this.props.profile) {
+      return (
+        <p>No profiles available.</p>
+      )
+    }
+    else {
+      return (
+        <div>
+        {this.renderProfilePicture()}
+        {this.renderContent()}
+        </div>
+      )
+    }
   }
 }
 
--- src/reducers/currentUserId.js
+++ src/reducers/currentUserId.js
@@ -2,6 +2,9 @@ import profileFixtures from '../fixtures/profiles';
 
 export default (state = profileFixtures[1].id, action = {}) => {
   switch (action.type) {
+    case 'TOGGLE_PROFILE':
+      // for now this is hidden for testing purposes, to switch from band to musician
+      return state === 7 ? 1: 7
     default:
       return state;
   }
