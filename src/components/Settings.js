41b216ccd5bba83b3d2c456bb9638a84ae0e08b5
Jiri Swen <jiri.ei@hotmail.com>
2018-11-07 16:57:36 +0100

    Adjusted the settings container so that it renders a ProfileCardContainer

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
+import ProfileCardsContainer from './ProfileCardContainer';
 
 class Settings extends Component {
   render() { 
     return ( <div>
-      <p>test settings</p>
+      <ProfileCardsContainer profile={this.props.profile} match={this.props.match} />
+      <button>Edit details</button>
+      <form>
+        <p>Add filter</p>
+        <input type="list" />
+        <input type="submit" />
+      </form>
     </div> );
   }
 }
