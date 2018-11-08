e1ba89a4dde2317459f095bc42a4d6304d640e15
Jiri Swen <jiri.ei@hotmail.com>
2018-11-08 19:02:11 +0100

    got the checkboxes working

--- src/components/SearchPreferences.js
+++ src/components/SearchPreferences.js
@@ -9,14 +9,28 @@ class SearchPreferences extends Component {
   }
 
   updateFilters() {
-    // Genres
+    // Genres, dit kan vast efficienter
     const genreFilters = this.props.userFilters.filter(filter => filter[0] === 'genres')
-    genreFilters.forEach(filter => {this.setState({ 'Rock': true })}) // Weer zo'n template-probleem!
-    this.setState()
+    console.log(genreFilters);
+    GENRES.forEach(filter => {
+      if (this.state[filter] && !genreFilters.find(filterArray => filterArray[2] === filter)) {
+        this.setState({ [filter]: false })
+      }
+    })
+    genreFilters.forEach(filterArray => {
+      if (!this.state[filterArray[2]]) {
+        this.setState({ [filterArray[2]]: true })
+      }
+    })
   }
 
   componentDidMount() {
     this.updateFilters()
+  }
+
+  handleGenreChange = (e) => {
+    this.props.onChangeGenre(e);
+    this.updateFilters();
   }
   
   render() { 
@@ -24,7 +38,7 @@ class SearchPreferences extends Component {
       <div>
         <p>Genres (has to include all checked boxes)</p>
         <form>
-          {GENRES.map(genre => <p key={genre}><input type="checkbox" value={genre} checked={this.state[genre] || false} onChange={this.props.onChangeGenre}/> {genre} </p>)}
+          {GENRES.map(genre => <p key={genre}><input type="checkbox" value={genre} checked={this.state[genre] || false} onChange={this.handleGenreChange}/> {genre} </p>)}
         </form>
       </div>
     </div> );
