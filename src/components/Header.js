5fa2fc9e62f038fe72f1d83effbe52f1da48da40
Sil Westerveld <git@silwesterveld.com>
2018-11-08 17:23:43 +0100

    Add icons for the menu in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,10 @@ import {Link} from 'react-router-dom';
 
 class Header extends Component {
   render() { 
-    return ( <div>
-      <Link to="/settings">Settings</Link>
-      <Link to="/profile-list">Profile List</Link>
-      <Link to="/matches">Matches</Link>
+    return ( <div className={'header-container'}>
+      <Link to="/settings"><img className="bt-menu-settings" src={require('../assets/bt_menu_settings_inactive.svg')}/></Link>
+      <Link to="/profile-list"><img className="bt-menu-profiles" src={require('../assets/bt_menu_logo_active.svg')}/></Link>
+      <Link to="/matches"><img className="bt-menu-matches" src={require('../assets/bt_menu_matches_inactive.svg')}/></Link>
     </div> );
   }
 }
