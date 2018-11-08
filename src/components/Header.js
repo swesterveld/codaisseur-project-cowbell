01f148d5de76cae3c4d69d9b7929001ae232ebba
Sil Westerveld <git@silwesterveld.com>
2018-11-08 23:20:09 +0100

    Finetuning header with menu buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import './Header.css'
 
 class Header extends Component {
   render() { 
-    return ( <div className={'header-container'}>
-      <Link to="/settings"><img className="bt-menu-settings" src={require('../assets/bt_menu_settings_inactive.svg')}/></Link>
-      <Link to="/profile-list"><img className="bt-menu-profiles" src={require('../assets/bt_menu_logo_active.svg')}/></Link>
-      <Link to="/matches"><img className="bt-menu-matches" src={require('../assets/bt_menu_matches_inactive.svg')}/></Link>
+    return ( <div id={'header-container'}>
+      <Link to="/settings"><img id="header-bt-settings" className="header-item" src={require('../assets/bt_menu_settings_inactive.svg')} alt="settings"/></Link>
+      <Link to="/profile-list"><img id="header-bt-profiles" className="header-item" src={require('../assets/bt_menu_logo_active.svg')} alt="profiles"/></Link>
+      <Link to="/matches"><img id="header-bt-matches" className="header-item" src={require('../assets/bt_menu_matches_inactive.svg')} alt="matches"/></Link>
     </div> );
   }
 }
