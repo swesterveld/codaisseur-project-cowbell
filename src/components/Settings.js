d755a84221ae6b22b3a33e3198cb9b142995fe89
Sil Westerveld <git@silwesterveld.com>
2018-11-09 21:22:34 +0100

    Get back to <Link> components

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -12,34 +12,30 @@ class Settings extends Component {
         </div>
         <div id={"setting-buttons"}>
           <div id={"settings-filters"} className={"settings-item"}>
-            {/*<Link to="/settings/filters">*/}
             <div>
-              <a href={"/settings/filters"}><img id={"settings-bt-filters"} src={require('../assets/bt_settings_filter.svg')} onClick={this.props.switchToContent} alt={"Configure Filters"}/></a>
+              <Link to="/settings/filters"><img id={"settings-bt-filters"} src={require('../assets/bt_settings_filter.svg')} onClick={this.props.switchToContent} alt={"Configure Filters"}/></Link>
             </div>
             <div>
               Search preferences
             </div>
-            {/*</Link>*/}
           </div>
           <div id={"settings-settings"} className={"settings-item"}>
-            {/*<Link to="/settings/detailed-settings">*/}
+
             <div id={"settings-bt-settings"}>
-              <a href={"/settings/detailed-settings"}><img src={require('../assets/bt_settings_settings.svg')} alt={"Settings"}/></a>
+              <Link to="/settings/detailed-settings"><img src={require('../assets/bt_settings_settings.svg')} alt={"Settings"}/></Link>
             </div>
             <div>
               Settings
             </div>
-            {/*</Link>*/}
           </div>
           <div id={"settings-profile"} className={"settings-item"}>
-            {/*<Link to="/settings/profile">*/}
+
             <div id={"settings-bt-profile"}>
-              <a href={"/settings/profile"}><img id={"settings_bt_profile"} src={require('../assets/bt_settings_edit_profile.svg')} onClick={this.props.switchToMedia} alt={"Edit Profile"}/></a>
+              <Link to="/settings/profile"><img id={"settings_bt_profile"} src={require('../assets/bt_settings_edit_profile.svg')} onClick={this.props.switchToMedia} alt={"Edit Profile"}/></Link>
             </div>
             <div>
               Edit profile
             </div>
-            {/*</Link>*/}
           </div>
         </div>
       </div>
