242075e661c5a6c38a92d3fe967b0daf6af85180
Cudles <39604742+Cudles@users.noreply.github.com>
2018-11-09 11:35:47 +0100

    Update profiles.js

--- src/fixtures/profiles.js
+++ src/fixtures/profiles.js
@@ -36,9 +36,9 @@ const profileFixtures = {
     name: 'Frank Sinatra',
     genres: ['Jazz', 'Pop'],
     description: 'Frank Sinatra\'s music is a blend of Jazz, traditional pop and swing music. He was easy listening, a little jazz, love songs and popular music. He will always be the Chairman of the Board and my very favorite singer.',
-    pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Frank_Sinatra_%2757.jpg/220px-Frank_Sinatra_%2757.jpg',
+    pictureURL: 'https://d2npu017ljjude.cloudfront.net/images/regular-43/w735/96303-11.jpg',
     location: 'Zaandam',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=25HQlRAP_3w',
       'https://open.spotify.com/track/1PVTvvxpSkyJWemW1CwVVk?si=uKFMguXVSfqlAp1cidlcJA'
@@ -55,7 +55,7 @@ const profileFixtures = {
     description: 'She is known for her unconventionality and provocative work, as well as visual experimentation. Gaga began performing as a teenager, singing at open mic nights and acting in school plays.',
     pictureURL: 'https://media.glamour.com/photos/59566919f4609a21dfa111fa/master/pass/lady-gaga-fashion-awards-getty-1.jpg',
     location: 'Hilversum',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://open.spotify.com/track/7dZ1Odmx9jWIweQSatnRqo?si=HiNSRyqESAiu9jo8apafwQ',
       'https://www.youtube.com/watch?v=bo_efYhYU2A'
@@ -70,9 +70,9 @@ const profileFixtures = {
     name: 'Kanye West',
     genres: ['Pop', 'Hip-hop', 'Soul'],
     description: 'West\'s musical career has been marked by dramatic changes in styles, incorporating an eclectic range of influences from soul, baroque-pop, electro, indie rock, synth-pop, industrial and gospel. Over the course of his career, West has been responsible for cultural movements and progressions within mainstream hip-hop and popular music at large. ',
-    pictureURL: 'https://www.billboard.com/files/styles/article_main_image/public/media/kanye-west-2015-a-billboard-1548.jpg',
+    pictureURL: 'http://www.nova.fr/sites/default/files/styles/ratio_1_370/public/episode/2018-08/Kanye_West_at_the_2009_Tribeca_Film_Festival.jpg?itok=oTCkUdl7',
     location: 'Nieuwegein',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=QpF_df5xpOI',
       'https://open.spotify.com/track/4S8d14HvHb70ImctNgVzQQ?si=tNFutkkoT9q4UWdPz4nSZw'
@@ -89,7 +89,7 @@ const profileFixtures = {
     description: 'The Son of God',
     pictureURL: 'https://www.biography.com/.image/t_share/MTM2OTI2NTkwNzE3MTQyNjIz/justin_bieber_2015_photo_courtesy_dfree_shutterstock_348418241jpg.jpg',
     location: 'Amsterdam',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=DK_0jXPuIr0',
       'https://open.spotify.com/track/50kpGaPAhYJ3sGmk6vplg0?si=LqeP_CdLRr2IwvfH_4CSDA'
@@ -106,7 +106,7 @@ const profileFixtures = {
     description: 'Beyoncé Giselle Knowles-Carter is an American singer, songwriter, producer, and actress. Born and raised in Houston, Texas, Beyoncé performed in various singing and dancing competitions as a child. She rose to fame in the late 1990s as lead singer of the R&B girl-group Destiny\'s Child.',
     pictureURL: 'https://images-na.ssl-images-amazon.com/images/I/A15dQM39ELL._CR0,0,3840,2880_._SL1000_.jpg',
     location:' Utrecht',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=PeonBmeFR8o'
     ],
@@ -122,7 +122,7 @@ const profileFixtures = {
     description: 'Saving the world, one clap a time.',
     pictureURL: 'https://images-na.ssl-images-amazon.com/images/I/A15dQM39ELL._CR0,0,3840,2880_._SL1000_.jpg',
     location: 'Amsterdam',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=ftjEcrrf7r0',
       'https://open.spotify.com/track/6wpGqhRvJGNNXwWlPmkMyO?si=EezJwK33RneEphYL8faglg'
@@ -137,9 +137,9 @@ const profileFixtures = {
     name: 'K3',
     genres: ['Pop'],
     description: 'K3 is a Belgian-Dutch girl group with a Dutch repertoire, whose current line-up is composed of Hanne Verbruggen, Marthe De Pillecyn and Klaasje Meijer, mainly aimed at pre-adolescent children.',
-    pictureURL: 'https://images-1.schellywood.be/thumbnail/hero-slider-image/1680/char-hero-image.png',
+    pictureURL: 'https://images-1.schellywood.be/thumbnail/book-image/17869/new-image.jpg',
     location: 'Brussels',
-    contactInfo: {whatsapp: '03463423', messenger: 'jiri.swen', email: 'jiri.ei@hotmail.com'},
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=zZ--w92ASdM',
       'https://www.youtube.com/watch?v=zo7ik2q8_4E'
@@ -151,9 +151,9 @@ const profileFixtures = {
     name: 'U2',
     genres: ['Pop', 'Rock'],
     description: 'U2 are an Irish rock band from Dublin formed in 1976. The group consists of Bono, the Edge, Adam Clayton, and Larry Mullen Jr.',
-    pictureURL: 'https://www.satelitemusical.net/sitebuilder/images/u2-2014-336x336.jpg',
+    pictureURL: 'https://cdn.ontourmedia.io/u2/non_secure/images/20170105/new_profile_crop1483638184/large.jpg',
     location: 'Amsterdam',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=ftjEcrrf7r0',
       'https://open.spotify.com/track/6wpGqhRvJGNNXwWlPmkMyO?si=EezJwK33RneEphYL8faglg'
@@ -165,14 +165,61 @@ const profileFixtures = {
     name: 'Tame Impala',
     genres: ['Rock', 'Indie'],
     description: 'Tame Impala is a psychedelic rock band conceived by Australian multi-instrumentalist Kevin Parker. The band began as a home-recording project for Parker, who writes, records, performs, and produces the music.',
-    pictureURL: 'https://img.discogs.com/MzwXGgJ1_G5L9IoP-TUO7ZtEahQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/A-1245513-1466491938-8401.jpeg.jpg',
+    pictureURL: 'https://e0af4153dabf8f9d6b2b-0afdb671d2fada65aba92528a1f1e10d.ssl.cf3.rackcdn.com/wp-content/uploads/2018/02/TameImpala-600x600.jpg?x61497',
     location: 'Rotterdam',
-    contactInfo: '',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
     recordingURLs: [
       'https://www.youtube.com/watch?v=sBzrzS1Ag_g'
     ],
     id: 9,
     wantedRoles: ['Bassist'],
+  }),
+    10: makeMusician({
+    name: 'John Frusciante',
+    genres: ['Rock'],
+    description: 'John Anthony Frusciante is an American guitarist, singer, composer, and producer. He is best known as the former guitarist of the rock band Red Hot Chili Peppers, from 1988 until 1992 and from 1998 until 2009. He recorded five studio albums with them.',
+    pictureURL: 'http://shop.borucki.com/wp-content/uploads/2013/03/BORUCKI_frusciante.jpg',
+    location: 'Hilversum',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
+    recordingURLs: [
+      'https://www.youtube.com/watch?v=m5RRDYPCuvA',
+      'https://open.spotify.com/track/2Lt6XpDCo3xETDwWgvZJIY?si=337JzOG2Q16BEZgc1MOsyg'
+    ],
+    id: 10,
+    roles: ['Guitarist'],
+    age: 49,
+    gender: 'male',
+    bandId: 'null'
+  }),
+    11: makeMusician({
+    name: 'Milan van Bovenkamp',
+    genres: ['Disco', 'Jazz'],
+    description: 'Disco Jesus - For all your parties and festivities.',
+    pictureURL: 'https://yt3.ggpht.com/a-/ACSszfG2iQOPI3cYHXG5pIOl_2o0TDC2FyFx5UMfJg=s900-mo-c-c0xffffffff-rj-k-no',
+    location: 'Amsterdam',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
+    recordingURLs: [
+      'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      'https://open.spotify.com/track/6ztstiyZL6FXzh4aG46ZPD?si=7t36jpQPS9m-jiTOj8bXGg'
+    ],
+    id: 11,
+    roles: ['Cowbell'],
+    age: 36,
+    gender: 'male',
+    bandId: 'null'
+  }),
+    12: makeBand({
+    name: 'Atomic Kitten',
+    genres: ['Pop'],
+    description: 'Atomic Kitten are a British pop girl group formed in Liverpool in 1998 whose current members are Natasha Hamilton and Liz McClarnon. The group was founded by Orchestral Manoeuvres in the Dark members Andy McCluskey and Stuart Kershaw, who served as principal songwriters during Atomic Kitten\'s early years.',
+    pictureURL: 'https://image.shutterstock.com/image-photo/small-brown-kitten-isolated-on-260nw-701715115.jpg',
+    location: 'Rotterdam',
+    contactInfo: {whatsapp: '31634629805', messenger: 'paul.middelkoop', email: 'paul.middelkoop@gmail.com'},
+    recordingURLs: [
+      'https://www.youtube.com/watch?v=EErY75MXYXI'
+    ],
+    id: 12,
+    wantedRoles: ['Singer'],
   })
 }
 
