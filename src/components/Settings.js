b9a0ebc9d8fb749bcac47cb37789949a43891c4b
Sil Westerveld <git@silwesterveld.com>
2018-11-09 12:44:59 +0100

    Add ProfilePicture to settings page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
+import ProfilePicture from './ProfilePicture'
 
 class Settings extends Component {
   render() { 
-    return ( <div>
-      <div> {/* moet vervangen worden door het ding met de CSS van ProfileCard */}
-        <img src={this.props.profile.pictureURL} alt="your profile" />
-        <p>{this.props.profile.name}, {this.props.profile.type === 'musician' && this.props.profile.age}</p>
-        <p>{this.props.profile.location}</p>
+    return (
+      <div>
+        <div>
+          <ProfilePicture profile={this.props.profile} />
+        </div>
+        <button><Link to="/settings/filters">Search preferences</Link></button>
+        <button><Link to="/settings/profile">Edit profile</Link></button>
+        <button><Link to="/settings/detailed-settings">Settings</Link></button>
       </div>
-      <button><Link to="/settings/filters">Search preferences</Link></button>
-      <button><Link to="/settings/profile">Edit profile</Link></button>
-      <button><Link to="/settings/detailed-settings">Settings</Link></button>
-    </div> );
+    );
   }
 }
 
