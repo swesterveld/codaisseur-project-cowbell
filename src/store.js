d7a4b13b2b2d281affff37f5ce31caa1095f1889
Sil Westerveld <git@silwesterveld.com>
2018-11-09 22:08:21 +0100

    Deactivate Redux Devtools

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './reducers';
 import ReduxThunk from 'redux-thunk';
 
-export const DEVELOPMENT = true
+export const DEVELOPMENT = false
 
 let store
 
